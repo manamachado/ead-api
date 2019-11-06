43d29984c356315d3249093f8241156d9a97681d
Maná Machado <manasses.machado@luizalabs.com>
2019-11-06 15:09:15 -0300

    chore: add eslint

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-'use strict';
-
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost'
+    host: 'localhost',
   });
 
   server.route({
     method: 'GET',
     path: '/',
-    handler: (request, h) => {
-
-      return 'Hello World!';
-    }
+    handler: () => 'Hello World!',
   });
 
   await server.start();
@@ -23,7 +17,6 @@ const init = async () => {
 };
 
 process.on('unhandledRejection', (err) => {
-
   console.log(err);
   process.exit(1);
 });
